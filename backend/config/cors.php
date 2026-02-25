08b04e45e1c749a46aabc1ab85557b7c7ea7ffff
Sherwin Adonis M. Vizcarra - II <adnis7222@gmail.com>
2026-02-25 21:38:13 +0800

    cors edited

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -10,15 +10,7 @@ return [
     'allowed_origins' => $allowedOrigins ?: [
         'http://localhost:5173',
         'http://127.0.0.1:5173',
-        'http://192.168.68.106:5173',
-        'http://10.74.218.253:5173',
-        'http://192.168.68.62:5173',
-        'http://192.168.68.61:5173',
-        'http://192.168.68.106:5173',
-        'http://10.0.31.10:5173',
-        'http://192.168.68.64:5173',
-        // 'https://dashboard-ncnian-id.svizcarra.online',
-        'https://ncnian-id.svizcarra.online',
+        'https://ncnian-id-form.svizcarra.online',
     ],
 
     'allowed_headers' => ['*'],
