8e1a12a2dce2a49872886ff64600be22a21d16ac
Sherwin Adonis M. Vizcarra - II <adnis7222@gmail.com>
2026-03-17 09:43:00 +0800

    Build 0.4B fixes on mgiration file

--- backend/database/migrations/2026_03_11_073916_add_extra_fields_and_archiving_to_students_table.php
+++ backend/database/migrations/2026_03_11_073916_add_extra_fields_and_archiving_to_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration 
 {
     /**
      * Run the migrations.
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('students', function (Blueprint $table) {
-            $table->string('reissuance_reason')->nullable()->after('payment_proof');
+            $table->string('reissuance_reason')->nullable();
             $table->string('manual_full_name')->nullable()->after('last_name');
             $table->boolean('is_archived')->default(false)->after('updated_at');
             $table->timestamp('archived_at')->nullable()->after('is_archived');
